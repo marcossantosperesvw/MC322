8ca484f4be4500665772d902683558f3951477a5
Marcos <marcos.santos.peresvw@gmail.com>
2025-09-11 15:29:17 -0300

    Adicao de atributo para monstro largar arma aleatoria

--- tarefa2/src/jogo/Monstro.java
+++ tarefa2/src/jogo/Monstro.java
@@ -4,9 +4,25 @@ import Armas.*;
 public abstract class Monstro extends Personagem{
     private int xp_concedido;
     private boolean atordoado = false;
+    private Arma[] listaArmasParaLargar;
     public Monstro(String nome, int pontos_de_vida, int forca, int xp_concedido, Arma arma){
         super(nome, pontos_de_vida, forca, arma);
         this.xp_concedido = xp_concedido;
+ 
+    }
+    public Arma[] getListaArmasParaLargar() {
+        return listaArmasParaLargar;
+    }
+    public void setListaArmasParaLargar(Arma arma1, Arma arma2, Arma arma3) {
+        this.listaArmasParaLargar[0] = arma1;
+        this.listaArmasParaLargar[1] = arma2;
+        this.listaArmasParaLargar[2] = arma3;
+    }
+    public Arma largarArma(){
+        int numero = (int)(Math.random() * 3);
+        Arma arma_largar = this.listaArmasParaLargar[numero];
+        this.listaArmasParaLargar[numero] = null;
+        return arma_largar;
     }
     public int getXpConcedido(){
         return this.xp_concedido;
