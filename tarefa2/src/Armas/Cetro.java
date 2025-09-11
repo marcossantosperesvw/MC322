971635c560945aa8f2f69c874a72742b608e79b5
CaireB <c281135@dac.unicamp.br>
2025-09-11 17:26:20 -0300

    Tentando

---
+++ tarefa2/src/Armas/Cetro.java
@@ -0,0 +1,7 @@
+package Armas;
+
+public class Cetro extends Arma{
+    public Cetro(String nome, int MinNivel, int dano){
+        super(nome, dano, MinNivel);
+    }   
+}
--- tarefa2/src/Armas/Lightning.java
+++
@@ -1,7 +0,0 @@
-package Armas;
-
-public class Lightning extends Arma{
-    public Lightning(String nome, int MinNivel, int dano){
-        super(nome, dano, MinNivel);
-    }   
-}
